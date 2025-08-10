470ecd3e2a266d3daf4dba5b65333cc21d2bac17
Krishna-MOR3 <krishna.more8200@gmail.com>
2025-08-10 17:38:08 +0530

    Update index.js and main.py

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
-    credentials: true
+   origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'http://localhost:5175',
+      'https://job-portal-eight-orcin.vercel.app'   // Add your Vercel frontend URL here
+    ],
 }
 
 app.use(cors(corsOptions));
--- genai-backend/main.py
+++ genai-backend/main.py
@@ -28,7 +28,8 @@ app = FastAPI(
 # Configure CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000",
+      'https://job-portal-eight-orcin.vercel.app'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
